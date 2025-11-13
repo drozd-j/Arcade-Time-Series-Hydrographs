0dec688fa14f645f30745e31b90516d7d8c07476
Jakob Drozd <jdrozd@esri.com>
2025-11-13 16:30:13 -0500

    Add files via upload

---
+++ arcade-hydrographs.js
@@ -0,0 +1,112 @@
+// date calculations
+var currDate = Now()
+var oneDay = DateAdd(currDate, -24, "hours")
+var sevDays = DateAdd(currDate, -7, "days")
+var thirDays = DateAdd(currDate, -30, "days")
+
+// create empty dictionary + arrays for all charts
+var attributess = {};
+var oneFieldInfos = [];
+var sevFieldInfos = [];
+var thirFieldInfos = [];
+
+// create fs for MO gages
+var fs = FeatureSetByRelationshipName($feature, "Gage_Readings");
+var oneFs = Filter(fs, 'Time >= @oneDay')
+var sevFs = Filter(fs, 'Time >= @sevDays')
+var thirFs = Filter(fs, 'Time >= @thirDays')
+
+// iterate through gages, pull stage and time, and create an associated fieldInfo to include it in the table
+
+//////////////// past day
+
+for (var item in oneFs) {
+
+  var oneFlow = item["Stage_ft"];
+  var oneTimex = Text(item["Time"], "M/DD/YYYY h:mm A");
+  attributess[oneTimex] = oneFlow;
+  Push(oneFieldInfos, oneTimex)
+}
+
+//////////////// 
+
+//////////////// past seven days
+
+for (var item in sevFs) {
+
+  var sevFlow = item["Stage_ft"];
+  var sevTimex = Text(item["Time"], "M/DD/YYYY h:mm A");
+  attributess[sevTimex] = sevFlow;
+  Push(sevFieldInfos, sevTimex)
+}
+
+//////////////// 
+
+//////////////// past thirty days
+
+for (var item in thirFs) {
+
+  var thirFlow = item["Stage_ft"];
+  var thirTimex = Text(item["Time"], "M/DD/YYYY h:mm A");
+  attributess[thirTimex] = thirFlow;
+  Push(thirFieldInfos, thirTimex)
+}
+
+////////////////
+
+If (!IsEmpty(thirFlow)){
+return {
+  type: "media",
+  attributes: attributess,
+  title: "",
+  mediaInfos: [
+    {
+      type: "linechart",
+      altText: "Recorded height in feet over the last 24 hours for Missouri Streamgages.",
+      title: "<p style='color:#2C486B;font-weight:300;margin:8px'>Last 24 Hours</p>",
+      value: {
+        fields: oneFieldInfos,
+        "colors": [
+          [
+            44,
+            72,
+            107
+          ]
+        ]
+      }
+    },
+    {
+      type: "linechart",
+      altText: "Recorded height in feet over the last 7 days for Missouri Streamgages.",
+      title: "<p style='color:#2C486B;font-weight:300;margin:8px'>Last 7 Days</p>",
+      value: {
+        fields: sevFieldInfos,
+        "colors": [
+          [
+            44,
+            72,
+            107
+          ]
+        ]
+      }
+    },
+    {
+      type: "linechart",
+      altText: "Recorded height in feet over the last 30 days for Missouri Streamgages.",
+      title: "<p style='color:#2C486B;font-weight:300;margin:8px'>Last 30 Days</p>",
+      value: {
+        fields: thirFieldInfos,
+        "colors": [
+          [
+            44,
+            72,
+            107
+          ]
+        ]
+      }
+    }
+  ]
+}}
+else {
+  return ("")
+}
